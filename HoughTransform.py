5b91640f473532014da95e3fad110c4f4806c7e6
hamza9305 <hamzaahmed9305@gmail.com>
2021-08-22 14:49:54 +0200

    Adding Files

---
+++ HoughTransform.py
@@ -0,0 +1,48 @@
+import numpy as np
+import cv2
+
+indir = 'Specify input directory'
+outdir = 'Specify output directory'
+
+# Read in and grayscale the image
+image = cv2.imread(indir)
+gray = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
+
+# Define a kernel size and apply Gaussian smoothing
+kernel_size = 5
+blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
+cv2.imwrite(outdir + '/' + 'blur_gray.jpg',blur_gray)
+
+# Define parameters for Canny
+low_threshold = 50
+high_threshold = 150
+masked_edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
+cv2.imwrite(outdir + '/' + 'masked_edges.jpg',masked_edges)
+
+# Define the Hough transform parameters
+# Make a blank the same size as our image to draw on
+rho = 1
+theta = np.pi/180
+threshold = 1
+min_line_length = 10
+max_line_gap = 1
+line_image = np.copy(image)*0 #creating a blank to draw lines on
+
+# Run Hough on edge detected image
+lines = cv2.HoughLinesP(masked_edges, rho, theta, threshold, np.array([]),
+                            min_line_length, max_line_gap)
+
+# Iterate over the output "lines" and draw lines on the blank
+for line in lines:
+    for x1,y1,x2,y2 in line:
+        cv2.line(line_image,(x1,y1),(x2,y2),(0,0,255),10)
+
+# Create a "color" binary image to combine with line image
+color_edges = np.dstack((masked_edges, masked_edges, masked_edges))
+
+# Draw the lines on the edge image
+combo = cv2.addWeighted(color_edges, 0.8, line_image, 1, 0)
+cv2.imshow('Combined',combo)
+cv2.waitKey()
+
+cv2.imwrite(outdir + '/' + 'combined.jpg',combo)
